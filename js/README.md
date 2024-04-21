9a15f9b0e9538f28a0446eb2da2cd80f5faca17a
Danny Gauthier <dgauthier@WKS-002169.corp.coveo.com>
2024-04-21 10:12:14 -0400

    feat(js basics): modules

---
+++ js/README.md
@@ -0,0 +1,6 @@
+# I-dont-know-JS
+Exercises in JS
+
+## How-to
+
+Install the latest node.js with nvm and run the scripts
---
+++ js/basics/modules/person.mjs
@@ -0,0 +1,24 @@
+"use strict";
+
+import createHuman from "./human.mjs";
+
+export default function createPerson(name, age) {
+  const human = createHuman();
+
+  function introduce() {
+    human.talk();
+    console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+  }
+  function listenToSomeone(person) {
+    human.hear();
+    console.log(`${name} is listening to`, person.name);
+  }
+
+  return {
+    ...human,
+    name,
+    age,
+    introduce,
+    listenToSomeone,
+  };
+}
